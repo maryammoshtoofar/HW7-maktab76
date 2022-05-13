50a29ef058a24add09647bf4e17a270c65dc130e
Maryam Moshtoofar <maryammoshtoofar@gmail.com>
2022-05-13 20:42:28 +0430

    final ex1

--- script1.js
+++ script1.js
@@ -1,7 +1,8 @@
-// Ex.1
-// Your task is to create a Circle constructor that creates a circle with a radius provided by an
-// argument. The circles constructed must have two methods getArea() (PI*r^2) and
-// getPerimeter() (2*PI*r) which give both respective areas and perimeter (circumference).
+// // Ex.1
+// // Your task is to create a Circle constructor that creates a circle with a radius provided by an
+// // argument. The circles constructed must have two methods getArea() (PI*r^2) and
+// // getPerimeter() (2*PI*r) which give both respective areas and perimeter (circumference).
+
 function Circle(r) {
   this.getArea = () => `Area = ${(Math.PI * Math.pow(r, 2)).toFixed(5)}`;
   this.getPerimeter = () => `Perimeter = ${(Math.PI * 2 * r).toFixed(5)}`;
